c7af55d14b65fda98aebd2efc298fa9cb62ed02c
Luka Žugaj <lzugaj@tvz.hr>
2020-02-07 09:42:41 +0100

    Initial commit

---
+++ src/test/java/com/luv2code/forumoverflow/ForumOverflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luv2code.forumoverflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForumOverflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
